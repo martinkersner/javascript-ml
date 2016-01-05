cbb87b29bf3d4b7c4892310f29a4e37e50ff4687
Martin Keršner <m.kersner@gmail.com>
2016-01-05 09:38:23 +0900

    partially finished computation of gradient descent

--- logistic_regression.js
+++ logistic_regression.js
@@ -1,22 +1,40 @@
+// TODO load csv
+//      unit tests
+//      real dataset
+
 function dummy_data() {
   var X = math.matrix([[1, 1], [0, 0]]);
-  var y = math.matrix([1, 0]);
+  var y = math.matrix([[1], [0]]);
 
-  return [y X];
+  return [X, y];
 }
 
 function sigmoid(z) {
   return 1/(1+math.exp(-z));
 }
 
-function gradient_descent() {
-  var cycles = 200;
-  var cols = 2;
-  var alpha = 0.001;
+function neg(x) {
+ return math.dotMultiply(x, -1)
+}
 
+function sigmoid_matrix(z) {
+  var ones = math.ones(z.size());
+  return math.dotDivide(ones, math.add(math.exp(neg(z)), ones));
+}
+
+function gradient_descent(X, y) {
+  var cycles = 50;
+  var alpha = 0.001;
+  var cols = X.size()[1];
   var w = math.ones(cols, 1);
+
+  var h, err;
+
   for (i = 0; i <= cycles; i++) {
-    var h = sigmoid(math.multiply(X, w));
-    var err = y - h;
+    h = sigmoid_matrix(math.multiply(X, w));
+    err = math.subtract(y, h);
+    w = math.add(w, math.multiply(X, math.dotMultiply(alpha, err)))
+
+    document.writeln(w)
   }
 }
